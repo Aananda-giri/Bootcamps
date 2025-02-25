43a1aa41751469441422d0977c4ccbc2673ed04d
aananda-giri <aanandaprashadgiri@gmail.com>
2025-02-25 18:53:59 +0545

    chapter 13

--- ch-13-Relating-Posts-with-User-Collection/controllers/home.js
+++ ch-13-Relating-Posts-with-User-Collection/controllers/home.js
@@ -4,6 +4,7 @@ module.exports = async(req, res) => {
     const blogposts = await BlogPost.find({}).populate('userid');
     console.log(req.session);
     res.render('index',{
-        blogposts
+        blogposts,
+        homePage: true
     });
 }
--- ch-13-Relating-Posts-with-User-Collection/controllers/newPost.js
+++ ch-13-Relating-Posts-with-User-Collection/controllers/newPost.js
@@ -1,7 +1,9 @@
 module.exports = (req, res) =>{
     console.log('calling newPost');
     if (req.session.userId){
-        res.render('create');
+        res.render('create', {
+            createPost: true
+        });
     } else {
         res.redirect('/auth/login');
     }
